2e1a4b809351f2df0ed2a7086d8d268b3de7aafe
woof <woof@huahua.wtf>
2022-11-23 20:06:30 +0000

    uploading source

--- README.md
+++ README.md
@@ -5,22 +5,73 @@ _An interchain upgrades watchdog brought to you by [ChihuahuaChain](https://chih
 
 # Info
 UpgradesWatchdog 🐶 makes it easier to follow any Software Upgrade Proposal and GitHub release of any Cosmos Blockchain.
-Collaborate by adding the chains you would want it to follow by adding them on the _chain_data.json_ file and send a PR.
+The first iteration of UpgradesWatchdog it's a rushed, working, draft.
+The bot uses the chain-registry data in order to search for new GitHub releases and scans Governance Voting Proposals to get SoftwareUpgrades info.
+
+#### We are currently supporting and tracking the following networks on the [Official Telegram Channel](https://t.me/CosmosUpgrades) checking for updates every 1 hour.
+- Akash
+- Assetmantle
+- Axelar
+- Bandchain
+- Bitcanna
+- Bitsong
+- Cerberus
+- Chihuahua (obviously)
+- Comdex
+- Cosmos Hub
+- Crescent
+- Crypto.org
+- Evmos
+- Gravity-Bridge
+- Injective
+- Juno
+- Omniflix Hub
+- Osmosis
+- Passage
+- Persistence
+- Secret Network
+- Sentinel (DVPN)
+- Stargaze
+- Teritori
+- Terra
+- Terra 2
+- Umee
+
+If you need us to follow a specific network, feel free to open a PR
 
 # Todo
-There's a lot more to add and polish, twitter? discord? The more, the better!
+This code is currently a draft and rushed in to "just work".
+There is a lot more to add and polish, twitter? discord? The more, the better!
+Some networks are giving us headache because they don't properly release software on GitHub or because of faulty endpoints.
 
-- [x] v1.0b Private Testing
-- [x] Release config file (waiting for PR)
-- [ ] Release Source Code
-- [ ] Add Twitter
-- [ ] Add Discord
+- [x] v2.0b Private Testing
+- [x] Release Source Code
+- [ ] Better coding
+- [ ] Add Twitter Notifications
+- [ ] Add Discord Notifications
 
 
 ## Installation
 
-As soon as the source code will be released, more info on how to install will be released.
+- Clone the repository
+```bash 
+git clone https://github.com/ChihuahuaChain/UpgradesWatchdog
+```
+- Install dependencies
+```bash 
+cd UpgradesWatchdog\
+pip3 install -m requirements.txt
+```
+- Edit the configuration variables on the watchdog.py
+  - ```github_token = "YOUR_GITHUB_TOKEN"```
+  - ```telegram_token = "TELEGRAM_BOT_TOKEN"```
+  - ```telegram_notification = ["TELEGRAM_ID_1", "TELEGRAM_ID_2"]```
+  - ```supported = ["chihuahua", "osmosis", "bitcanna"]```
 
+- Launch it
+  - ```python3 watchdog.py```
+
+If you want to automatically run it at any given time, use crontab.
 
 # Donate
 We don't seek for donations, but you can say Thank You for our work by [delegating to our validators](https://delegate.chihuahua.wtf) and by [sharing this project on Twitter](https://twitter.com/intent/tweet?text=Check%20out%20%23pyCosmicWrap%20%F0%9F%8C%AF%20by%20%40ChihuahuaChain%20-%20A%20%23python%20wrapper%20for%20%40cosmos%20on%20https%3A//github.com/ChihuahuaChain/pyCosmicWrap%20%23HUAHUA%20%23Chihuahua%20%23WOOF%0A)
---
+++ watchdog.py
@@ -0,0 +1,149 @@
+# Upgrades Watchdog v2.0b
+# ChihuahuaChain - https://github.com/ChihuahuaChain/UpgradesWatchdog
+#
+# License GNU General Public License v3.0
+#
+# https://choosealicense.com/licenses/gpl-3.0/
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pycosmicwrap import CosmicWrap
+from telegram.ext import Updater
+from github import Github
+from git.repo.base import Repo
+from time import sleep
+import logging
+import json
+import os
+import re
+
+# CONFIGURATION
+
+# GitHub Personal Access Token
+github_token = "YOUR_GITHUB_TOKEN"
+
+# Telegram Bot API Token
+telegram_token = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# Comma separated Telegram chat_id, channels, groups to notify
+telegram_notification = ["@CosmosUpgrades"]
+
+# Comma separated supported chains
+supported = ["chihuahua", "osmosis", "bitcanna"]
+
+# CONFIGURATION END
+
+
+chains = []
+g = Github(github_token)
+chain_registry = "https://github.com/cosmos/chain-registry"
+telegram = Updater(token=telegram_token)
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+# Clone the chain-registry, if it's already there, update it.
+try:
+    Repo.clone_from(chain_registry, "./data/chain-registry")
+    logging.info("[!] Cloned https://github.com/cosmos/chain-registry into ./data")
+except:
+    Repo("./data/chain-registry").remotes.origin.pull()
+    logging.info("[!] Updated chain-registry data")
+
+
+for dirs in os.listdir("./data/chain-registry/"):
+    if dirs in supported:
+        chains.append(dirs)
+
+for chain in chains:
+    with open("./data/chain-registry/%s/chain.json" % chain, 'r') as chain_file:
+        chain = json.load(chain_file)
+    try:
+        with open('./data/cache/' + chain['chain_name'].lower() + '.json', 'r') as cache_file:
+            cache = json.load(cache_file)
+    except:
+        cache = {"last_notified_proposal": 0,
+                 "last_notified_release": "0"}
+    try:
+        for endpoint in chain['apis']['rest']:
+            if "https" in endpoint['address']:
+                logging.info("[?] (%s) Trying %s ..." % (chain['chain_name'].upper(), endpoint['address']))
+                try:
+                    connector = CosmicWrap(lcd=endpoint['address'], rpc='', denom='')
+                    proposals = connector.query_proposals()
+                    for proposal in proposals:
+                        if int(cache['last_notified_proposal']) < int(proposal['proposal_id']):
+                            if "SoftwareUpgradeProposal" in proposal['content']['@type'] \
+                                    and proposal['status'] == "PROPOSAL_STATUS_VOTING_PERIOD":
+                                upgrade_title = proposal['content']['title']
+                                upgrade_name = proposal['content']['plan']['name']
+                                upgrade_height = proposal['content']['plan']['height']
+                                proposal_id = proposal['proposal_id']
+                                for explorer in chain['explorers']:
+                                    if "mintscan" in explorer['kind']:
+                                        explorer_url = explorer['url']
+                                        break
+                                    else:
+                                        explorer_url = explorer['url']
+                                logging.info("[!] (%s) - Upgrade %s scheduled for block %s" %
+                                             (chain['chain_name'].upper(),
+                                              upgrade_name,
+                                              upgrade_height))
+                                message = "⚠️ <b>NEW SOFTWARE UPGRADE PROPOSAL</b>\n\n" \
+                                          "<b>Chain:</b> %s\n" \
+                                          "<b>Name:</b> %s\n" \
+                                          "<b>Height: </b><a href='%s/blocks/%s'>%s</a>\n\n" \
+                                          "🔗 <a href='%s/proposals/%s'>Check Proposal</a>\n" \
+                                          % (chain["chain_name"], upgrade_name, explorer_url, upgrade_height,
+                                             upgrade_height, explorer_url, proposal_id)
+
+                                for chat_id in telegram_notification:
+                                    try:
+                                        telegram.bot.sendMessage(chat_id=chat_id,
+                                                                 text=message,
+                                                                 parse_mode='HTML',
+                                                                 disable_web_page_preview=True)
+                                        cache['last_notified_proposal'] = proposal_id
+                                        with open('./data/cache/' + chain['chain_name'].lower() + '.json',
+                                                  'w') as cache_file:
+                                            json.dump(cache, cache_file)
+                                        sleep(.5)
+                                    except Exception as e:
+                                        logging.error("[X] Unable to send notification to %s. (%s)" % (chat_id, str(e)))
+
+                    # Checking GitHub Repository
+                    repository = g.get_repo(re.sub('.git', '', chain['codebase']['git_repo'][19:].rstrip("/")))
+                    releases = repository.get_latest_release()
+                    if releases.prerelease is False and cache['last_notified_release'] != releases.tag_name:
+                        message = "⚠️<b> NEW GITHUB RELEASE</b>️\n\n" \
+                                  "<b>Chain:</b> %s\n" \
+                                  "<b>Version:</b> %s\n" \
+                                  "<b>Release Date:</b> %s\n\n" \
+                                  "🔗 <a href='%s'>Check Release on Github</a>" \
+                                  % (chain['chain_name'], str(releases.tag_name),
+                                     str(releases.published_at), str(releases.html_url))
+                        for chat_id in telegram_notification:
+                            try:
+                                telegram.bot.sendMessage(chat_id=chat_id,
+                                                         text=message,
+                                                         parse_mode='HTML',
+                                                         disable_web_page_preview=True)
+                                sleep(.5)
+                                cache['last_notified_release'] = str(releases.tag_name)
+                                with open('./data/cache/' + chain['chain_name'].lower() + '.json', 'w') as cache_file:
+                                    json.dump(cache, cache_file)
+                            except Exception as e:
+                                logging.error("[X] Unable to send notification to %s. (%s)" % (chat_id, str(e)))
+                        logging.info(
+                            "[!] (%s) New version available: %s %s %s" % (
+                                chain['chain_name'].upper(), releases.tag_name,
+                                releases.published_at, releases.html_url))
+                    break
+                except Exception as e:
+                    logging.error("[X] (%s) ERROR %s" % (chain['chain_name'].upper(), str(e)))
+    except Exception as e:
+        logging.error("[X] (%s) ERROR %s" % (chain['chain_name'].upper(), str(e)))
