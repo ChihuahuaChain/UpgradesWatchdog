2573781d45ac9315fd00030b0245f9d1ee77854b
woof <woof@huahua.wtf>
2022-11-27 12:33:24 +0000

    updating to v2.2

--- watchdog.py
+++ watchdog.py
@@ -1,4 +1,4 @@
-# Upgrades Watchdog v2.0b
+# Upgrades Watchdog v2.2
 # ChihuahuaChain - https://github.com/ChihuahuaChain/UpgradesWatchdog
 #
 # License GNU General Public License v3.0
@@ -13,10 +13,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from PIL import ImageFont, Image, ImageDraw
 from pycosmicwrap import CosmicWrap
 from telegram.ext import Updater
 from git.repo.base import Repo
 from github import Github
+from io import BytesIO
 from time import sleep
 import multiprocessing
 import logging
@@ -38,8 +40,10 @@ telegram_notification = ["@CosmosUpgrades"]
 # Comma separated supported chains
 supported = ["chihuahua", "osmosis", "bitcanna"]
 
-# CONFIGURATION END
+# Send images instead of text, cool eh?
+use_image = True
 
+# CONFIGURATION END
 
 chains = []
 queue = []
@@ -47,6 +51,13 @@ g = Github(github_token)
 chain_registry = "https://github.com/cosmos/chain-registry"
 telegram = Updater(token=telegram_token)
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+
+def font(var, size):
+    if var == 'regular':
+        return ImageFont.truetype(os.getcwd() + "/fonts/Montserrat-Regular.ttf", size)
+    if var == 'bold':
+        return ImageFont.truetype(os.getcwd() + "/fonts/Montserrat-Bold.ttf", size)
 
 
 # Clone the chain-registry, if it's already there, update it.
@@ -64,6 +75,7 @@ def get_chain_data():
 
 
 def check_chain(chain):
+    global use_image
     with open(os.getcwd() + "/data/chain-registry/%s/chain.json" % chain, 'r') as chain_file:
         chain = json.load(chain_file)
     try:
@@ -72,6 +84,13 @@ def check_chain(chain):
     except:
         cache = {"last_notified_proposal": 0,
                  "last_notified_release": "0"}
+    try:
+        with open(os.getcwd() + '/data/chain-registry/' + chain['chain_name'].lower() + '/assetlist.json', 'r') as assetlist:
+            assetlist = json.load(assetlist)
+        chain_logo = os.getcwd() + "/data/chain-registry/%s" % assetlist['assets'][0]['logo_URIs']['png'][62:]
+    except KeyError:
+        chain_logo = False
+
     try:
         for endpoint in chain['apis']['rest']:
             if "https" in endpoint['address']:
@@ -107,10 +126,41 @@ def check_chain(chain):
 
                                 for chat_id in telegram_notification:
                                     try:
-                                        telegram.bot.sendMessage(chat_id=chat_id,
-                                                                 text=message,
-                                                                 parse_mode='HTML',
-                                                                 disable_web_page_preview=True)
+                                        if use_image is True:
+                                            img = Image.new("RGB", (600, 350), (240, 240, 240))
+                                            if chain_logo is not False:
+                                                logo = Image.open(chain_logo).convert("RGBA")
+                                                logo.thumbnail((120, 120))
+                                                converted_logo = Image.new("RGBA", (120, 120), (240, 240, 240))
+                                                converted_logo.paste(logo, (0, 0), logo)
+                                                converted_logo.convert('RGB')
+                                                img.paste(converted_logo, (20, 20))
+                                            draw = ImageDraw.Draw(img)
+                                            draw.text((160, 40), "New Upgrade Proposal",
+                                                      (0, 0, 0), font=font('bold', 32))
+                                            draw.text((120, 160), "Network: ", (0, 0, 0), font=font('regular', 26))
+                                            draw.text((120, 210), "Name: ", (0, 0, 0), font=font('regular', 26))
+                                            draw.text((120, 260), "Height: ", (0, 0, 0), font=font('regular', 26))
+                                            draw.text((250, 160), chain['chain_name'].upper(), (0, 0, 0),
+                                                      font=font('bold', 26))
+                                            draw.text((250, 210), upgrade_name, (0, 0, 0),
+                                                      font=font('bold', 26))
+                                            draw.text((250, 260), upgrade_height, (0, 0, 0),
+                                                      font=font('bold', 26))
+                                            bio = BytesIO()
+                                            bio.name = "output.jpeg"
+                                            img.save(bio, "JPEG")
+                                            bio.seek(0)
+                                            caption = "🔗 <a href='%s/blocks/%s'>Check Upgrade Countdown</a>\n" \
+                                                      "⏳ <a href='%s/proposals/%s'>Check GOV Proposal</a>" \
+                                                      % (explorer_url, upgrade_height, explorer_url, proposal_id)
+                                            telegram.bot.sendPhoto(chat_id=chat_id, photo=bio, caption=caption,
+                                                                   parse_mode="HTML")
+                                        else:
+                                            telegram.bot.sendMessage(chat_id=chat_id,
+                                                                     text=message,
+                                                                     parse_mode='HTML',
+                                                                     disable_web_page_preview=True)
                                         cache['last_notified_proposal'] = proposal_id
                                         with open(os.getcwd() + '/data/cache/' + chain['chain_name'].lower() + '.json',
                                                   'w') as cache_file:
@@ -132,16 +182,43 @@ def check_chain(chain):
                                      str(releases.published_at), str(releases.html_url))
                         for chat_id in telegram_notification:
                             try:
-                                telegram.bot.sendMessage(chat_id=chat_id,
-                                                         text=message,
-                                                         parse_mode='HTML',
-                                                         disable_web_page_preview=True)
+                                if use_image is True:
+                                    img = Image.new("RGB", (600, 350), (240, 240, 240))
+                                    if chain_logo is not False:
+                                        logo = Image.open(chain_logo).convert("RGBA")
+                                        logo.thumbnail((120, 120))
+                                        converted_logo = Image.new("RGBA", (120, 120), (240, 240, 240))
+                                        converted_logo.paste(logo, (0, 0), logo)
+                                        converted_logo.convert('RGB')
+                                        img.paste(converted_logo, (20, 20))
+                                    draw = ImageDraw.Draw(img)
+                                    draw.text((160, 40), "New GitHub Release",
+                                              (0, 0, 0), font=font('bold', 38))
+                                    draw.text((120, 160), "Network: ", (0, 0, 0), font=font('regular', 26))
+                                    draw.text((120, 210), "Version: ", (0, 0, 0), font=font('regular', 26))
+                                    draw.text((120, 260), "Released: ", (0, 0, 0), font=font('regular', 26))
+                                    draw.text((250, 160), chain['chain_name'].upper(), (0, 0, 0), font=font('bold', 26))
+                                    draw.text((250, 210), str(releases.tag_name), (0, 0, 0), font=font('bold', 26))
+                                    draw.text((250, 260), str(releases.published_at) + " UTC", (0, 0, 0), font=font('bold', 26))
+                                    bio = BytesIO()
+                                    bio.name = "output.jpeg"
+                                    img.save(bio, "JPEG")
+                                    bio.seek(0)
+                                    caption = "🔗 <a href='%s'>Check Release on Github</a>" % str(releases.html_url)
+                                    telegram.bot.sendPhoto(chat_id=chat_id, photo=bio, caption=caption, parse_mode="HTML")
+                                else:
+                                    telegram.bot.sendMessage(chat_id=chat_id,
+                                                             text=message,
+                                                             parse_mode='HTML',
+                                                             disable_web_page_preview=True)
                                 sleep(.5)
                                 cache['last_notified_release'] = str(releases.tag_name)
-                                with open(os.getcwd() + '/data/cache/' + chain['chain_name'].lower() + '.json', 'w') as cache_file:
+                                with open(os.getcwd() + '/data/cache/' + chain['chain_name'].lower() + '.json',
+                                          'w') as cache_file:
                                     json.dump(cache, cache_file)
                             except Exception as e:
                                 logging.error("[X] Unable to send notification to %s. (%s)" % (chat_id, str(e)))
+                                print(str(e))
                         logging.info(
                             "[!] (%s) New version available: %s %s %s" % (
                                 chain['chain_name'].upper(), releases.tag_name,
@@ -163,3 +240,4 @@ for process in queue:
 
 for process in queue:
     process.join()
+
